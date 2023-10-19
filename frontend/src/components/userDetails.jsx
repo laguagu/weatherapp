124263dd2cd6a63ed1a3c23658a6cef105b34807
laguagu <49702497+laguagu@users.noreply.github.com>
2023-10-19 21:15:41 +0300

    UserDetails for not authenticated user

--- frontend/src/components/userDetails.jsx
+++ frontend/src/components/userDetails.jsx
@@ -2,8 +2,8 @@ import { Alert, Button, Grid, Paper, Typography } from "@mui/material";
 import { sharedPaperStyle } from "../theme";
 
 function UserDetails() {
+  
   let userLoggedIn = false;
-  console.log();
 
   return (
     <Grid item xs={6}>
@@ -34,24 +34,18 @@ function UserDetails() {
           </>
         ) : (
           <>
-            <Typography color={"primary"} align="center" gutterBottom>
-              Hello Seppo
-            </Typography>
-            <Grid container spacing={3}>
-              <Grid item xs={6}>
-                <Alert severity="info">Sinulta loppu rahat</Alert>
-              </Grid>
-
-              <Grid item xs={6}>
-                <Alert severity="info">Sinulta loppu rahat</Alert>
-              </Grid>
-              <Grid item xs={12}>
-                <Typography color={"primary"} align="center" gutterBottom>
-                  <Button variant="contained">Logout</Button>
-                </Typography>
-              </Grid>
+          <Typography color={"primary"} align="center" gutterBottom>
+            User not logged in
+          </Typography>
+          <Grid container spacing={2} alignItems={"center"} justifyContent={"center"}>
+            <Grid item>
+              <Button variant="contained">Log in</Button>
             </Grid>
-          </>
+            <Grid item>
+              <Button variant="contained">Register</Button>
+            </Grid>
+          </Grid>
+        </>
         )}
       </Paper>
     </Grid>
