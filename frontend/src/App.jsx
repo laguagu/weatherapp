7ef52911bbfcc581545342ff3ea98b1befe79da2
laguagu <49702497+laguagu@users.noreply.github.com>
2023-10-19 11:47:07 +0300

    modified Grid elements added react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
-import { ThemeProvider } from '@emotion/react'
-import Home from './pages/Home'
-import theme from './theme'
+import { ThemeProvider } from "@emotion/react";
+import Home from "./pages/Home";
+import theme from "./theme";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+  ]);
 
   return (
     <ThemeProvider theme={theme}>
-      <Home/>
+      <RouterProvider router={router}>
+        <Home />
+      </RouterProvider>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/WeatherGrids.jsx
+++ frontend/src/components/WeatherGrids.jsx
@@ -2,42 +2,43 @@ import { Container, Grid, Paper, Typography, Divider } from "@mui/material";
 import { sharedPaperStyle } from "../theme";
 import { useEffect, useState } from "react";
 import fetchWeather from "../api/fetchWeather";
-import AirIcon from '@mui/icons-material/Air';
+import AirIcon from "@mui/icons-material/Air";
 
 function WeatherGrids() {
   const [weatherCity, setWeatherCity] = useState("");
-  const [weatherIcon, setWeatherIcon] = useState("")
-  const [weatherTemp, setWeatherTemp] = useState({celsius:"", feels_like: ""})
-  const [weatherInfo, setWeatherInfo] = useState("")
-  const [weatherWind, setWeatherWind] = useState("")
-  const [weatherhumidity, setWeatherhumidity] = useState("")
-  const [weatherPressure, setWeatherPressure] = useState("")
-  
+  const [weatherIcon, setWeatherIcon] = useState("");
+  const [weatherTemp, setWeatherTemp] = useState({
+    celsius: "",
+    feels_like: "",
+  });
+  const [weatherInfo, setWeatherInfo] = useState("");
+  const [weatherWind, setWeatherWind] = useState("");
+  const [weatherhumidity, setWeatherhumidity] = useState("");
+  const [weatherPressure, setWeatherPressure] = useState("");
+
   useEffect(() => {
     async function getWeather() {
       const result = await fetchWeather("Helsinki");
-      const icon = result.weather[0].icon
-      const temp = result.main.temp
-      const city = result.name
-      const info = result.weather[0].main
-      const wind = result.wind.speed
-      const humidity = result.main.humidity
-      const pressure = result.main.pressure
+      const icon = result.weather[0].icon;
+      const temp = result.main.temp;
+      const city = result.name;
+      const info = result.weather[0].main;
+      const wind = result.wind.speed;
+      const humidity = result.main.humidity;
+      const pressure = result.main.pressure;
       setWeatherCity(city);
-      setWeatherIcon(`https://openweathermap.org/img/wn/${icon}@2x.png`)
-      setWeatherTemp({celsius: temp, feels_like: result.main.feels_like})
-      setWeatherInfo(info)
-      setWeatherWind(wind)
-      setWeatherhumidity(humidity)
-      setWeatherPressure(pressure)
+      setWeatherIcon(`https://openweathermap.org/img/wn/${icon}@2x.png`);
+      setWeatherTemp({ celsius: temp, feels_like: result.main.feels_like });
+      setWeatherInfo(info);
+      setWeatherWind(wind);
+      setWeatherhumidity(humidity);
+      setWeatherPressure(pressure);
     }
     getWeather();
   }, []);
   return (
-    // Tai maxWidth="xl"
     <Container maxWidth="lg">
-
-      <img src={weatherIcon}/>
+      <img src={weatherIcon} />
       <Grid container spacing={5}>
         {/* Otsikko teksti ylälaatikon päällä*/}
         <Grid item xs={12}>
@@ -47,27 +48,38 @@ function WeatherGrids() {
         </Grid>
 
         {/* Ylälaatikko */}
-        <Grid item xs={12} >
-          <Paper elevation={3} sx={sharedPaperStyle} >
+        <Grid item xs={12}>
+          <Paper elevation={3} sx={sharedPaperStyle}>
             {/* Lisää Laatikoita ylälaatikon sisällä */}
             <Grid container spacing={50}>
-
               <Grid item xs={6}>
                 <Typography variant="h2" color="primary">
                   {weatherCity}
                 </Typography>
-                <Typography color="secondary" variant="h5" sx={{ marginTop: "15px" }}>
-                  Lämpötila: 
-                {weatherTemp.celsius > 0 ? " +":" -" }{weatherTemp.celsius+"°C"}
-                <Typography>Feels Like {weatherTemp.feels_like +"°C"}</Typography>
+                <Typography
+                  color="secondary"
+                  variant="h5"
+                  sx={{ marginTop: "15px" }}
+                >
+                  Lämpötila:
+                  {weatherTemp.celsius > 0 ? " +" : " -"}
+                  {weatherTemp.celsius + "°C"}
+                  <Typography>
+                    Feels Like {weatherTemp.feels_like + "°C"}
+                  </Typography>
                 </Typography>
               </Grid>
 
               <Grid item xs={6}>
-                <Typography color="primary" variant="h4" sx={{ marginTop: "15px" }}>{weatherInfo}</Typography>
-                <img src={weatherIcon}/>
+                <Typography
+                  color="primary"
+                  variant="h4"
+                  sx={{ marginTop: "15px" }}
+                >
+                  {weatherInfo}
+                </Typography>
+                <img src={weatherIcon} />
               </Grid>
-              
             </Grid>
           </Paper>
         </Grid>
@@ -79,34 +91,43 @@ function WeatherGrids() {
           </Typography>
           <Paper elevation={3} sx={sharedPaperStyle}>
             {/* Container kolmelle pienelle laatikolle */}
+
             <Grid container spacing={5}>
               <Grid item xs={4}>
                 <Paper sx={{ bgcolor: "black.main" }} elevation={2}>
-                  <Typography color="primary" align="center"> 
-                    Humidity 
-                    <img src="./src/images/dry.png"/>
-                    <Divider />
+                  <Typography color="primary" align="center">
+                    Humidity
+                    <img src="./src/images/dry.png" />
+                  </Typography>
+                  <Divider />
+                  <Typography color="primary" align="center">
                     {weatherhumidity + " %"}
                   </Typography>
                 </Paper>
               </Grid>
+
               <Grid item xs={4}>
                 <Paper sx={{ bgcolor: "black.main" }} elevation={2}>
                   <Typography color="primary" align="center">
-                  Tuuli
-                  <img src="./src/images/wind.png"/>
-                  {/* <AirIcon fontSize="large"/> */}
+                    Tuuli
+                    <img src="./src/images/wind.png" />
+                    {/* <AirIcon fontSize="large"/> */}
+                  </Typography>
                   <Divider />
-                  {weatherWind + " m/s"}
+                  <Typography color="primary" align="center">
+                    {weatherWind + " m/s"}
                   </Typography>
                 </Paper>
               </Grid>
+
               <Grid item xs={4}>
                 <Paper sx={{ bgcolor: "black.main" }} elevation={2}>
-                <Typography color="primary" align="center"> 
+                  <Typography color="primary" align="center">
                     Pressure
-                    <img src="./src/images/press.png"/>
-                    <Divider />
+                    <img src="./src/images/press.png" />
+                  </Typography>
+                  <Divider />
+                  <Typography color="primary" align="center">
                     {weatherPressure + " hPa"}
                   </Typography>
                 </Paper>
@@ -120,8 +141,11 @@ function WeatherGrids() {
           <Typography variant="h4" color="primary">
             Sää inffoa
           </Typography>
+
           <Paper elevation={3} sx={sharedPaperStyle}>
-            <Typography color="primary">Oikean</Typography>
+            <Typography color="primary" align="center">
+              Oikean
+            </Typography>
           </Paper>
         </Grid>
       </Grid>
