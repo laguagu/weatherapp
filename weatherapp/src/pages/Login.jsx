c465b9603462b1fff2c2a6954d2981e5ec2c5583
laguagu <49702497+laguagu@users.noreply.github.com>
2023-10-24 22:23:38 +0300

    Setting error message if username or password is wrong

--- weatherapp/src/pages/Login.jsx
+++ weatherapp/src/pages/Login.jsx
@@ -1,57 +1,71 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Box, TextField, Typography, Container, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { loginUser } from '../api/usersApi';
+import { loginUser } from "../api/usersApi";
 
 function Login() {
-    const [username, setUserName] = useState("")
-    const [password, setPassword] = useState("")
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleLogin = async () => {
+    const userDetails = {
+      username: username,
+      password: password,
+    };
+    console.log(userDetails);
+    const logginResult = await loginUser(userDetails);
 
-    const handleLogin = async () => {
-        const userDetails = {
-            username: username,
-            password: password
-        }
-        console.log(userDetails)
-        const logginResult = await loginUser(userDetails)
-        
-        if (logginResult.success) {
-            console.log("Onnistui")
-            navigate("/");
-        } else {
-            console.error("Kirjautuminen epäonnistui", result.error);
-        }
+    if (logginResult.success) {
+      console.log("Onnistui");
+      navigate("/");
+    } else {
+      setError("Wrong username or password");
+      setTimeout(() => {
+        setError("")
+      }, 4000);
     }
+  };
 
-    return (
-        <Container maxWidth="xs">
-            <Typography variant="h4" gutterBottom align='center'>Sign in</Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, marginTop:"20px"}}>
-                <TextField 
-                    id="username" 
-                    label="Käyttäjänimi" 
-                    variant="outlined"
-                    value={username}
-                    onChange={(e) => setUserName(e.target.value)}
-                />
-                <TextField 
-                    id="password" 
-                    label="Salasana" 
-                    type="password" 
-                    autoComplete="current-password" 
-                    variant="outlined"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <Button variant="contained" color="primary" onClick={handleLogin} > 
-                {/* component={Link} to="/" */}
-                    Sign in
-                </Button>
-            </Box>
-        </Container>
-    )
+  return (
+    <Container maxWidth="xs">
+      <Typography variant="h4" gutterBottom align="center">
+        Sign in
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          marginTop: "20px",
+        }}
+      >
+        <TextField
+          id="username"
+          label="Käyttäjänimi"
+          variant="outlined"
+          value={username}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <TextField
+          id="password"
+          label="Salasana"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button variant="contained" color="primary" onClick={handleLogin}>
+          {/* component={Link} to="/" */}
+          Sign in
+        </Button>
+        {error && <h1>{error}</h1>}
+      </Box>
+    </Container>
+  );
 }
 
 export default Login;
